3f90b0387b38e1501801bb1e13612f0cf4641a96
Dimitri Belopopsky <dimitri@belopopsky.com>
2018-06-09 13:16:34 +0200

    Added input and basic background color rendering.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,12 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
   glViewport(0, 0, width, height);
 }
 
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main() {
   glfwInit();
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -16,8 +22,8 @@ int main() {
   //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 
-  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-  if (window == NULL)
+  GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+  if (window == nullptr)
     {
       std::cout << "Failed to create GLFW window" << std::endl;
       glfwTerminate();
@@ -25,7 +31,7 @@ int main() {
     }
   glfwMakeContextCurrent(window);
 
-  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
     {
       std::cout << "Failed to initialize GLAD" << std::endl;
       return -1;
@@ -38,8 +44,13 @@ int main() {
 
   while(!glfwWindowShouldClose(window))
     {
-      glfwSwapBuffers(window);
+      processInput(window);
+
+      glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT);
+
       glfwPollEvents();
+      glfwSwapBuffers(window);
     }
 
   glfwTerminate();
